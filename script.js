922f462303db4855af8a685c063689822960d26e
SorcereR088 <sachinmhzn7@gmail.com>
2023-08-18 12:24:49 +0545

    added comments to js

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
-
+// Get the form element with the ID "myform"
 var form = document.getElementById("myform");
 
+// Add a submit event listener to the form
 form.addEventListener("submit", function (e) {
+  // Prevent the default form submission behavior
   e.preventDefault();
 
+  // Get the value of the search input field
   var search = document.getElementById("search").value;
 
+  // Fetch data from the GitHub API based on the entered username
   fetch("https://api.github.com/users/" + search)
     .then((result) => {
+      // Check if the response is not successful
       if (!result.ok) {
+        // Throw an error message indicating user not found
         throw new Error(`<br><br><br><p style=" text-align: center;
         color: white;
         text-transform: uppercase;
         font-family: 'Courier New', Courier, monospace;
         font-weight: bold;">User not found!!</p>`);
       }
+      // Parse the response as JSON and return the result
       return result.json();
     })
     .then((data) => {
+      // Log the fetched data to the console
       console.log(data);
 
+      // Update the HTML content of the "result" element with user data
       document.getElementById("result").innerHTML = `
       <link rel="stylesheet" href="style.css" />
       <br><br>
@@ -35,6 +44,7 @@ form.addEventListener("submit", function (e) {
       `;
     })
     .catch(error => {
+      // Handle errors by updating the HTML content with an error message
       document.getElementById("result").innerHTML = `<p class="error">${error.message}</p>`;
     });
 });
